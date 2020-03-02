08ec4bb21636edd1ce71b1851830e590716b7926
KarolBrzazgacz <61661483+KarolBrzazgacz@users.noreply.github.com>
2020-03-02 22:59:36 +0100

    Add files via upload

---
+++ DB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Lab1
+{
+    class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM Customers";
+            var command = new SqlCommand(sql, connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]);
+            }
+        }
+        public void Insert(SqlConnection connection, int id, string regionName)
+        {
+            var sql = "INSERT INTO  Region(RegionID, RegionDescription) VALUES (@id, '@regionName')";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@regionName", regionName);
+
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows inserted");
+        }
+        public void Delete(SqlConnection connection, int id, string regionName)
+        {
+            var sql = "DELETE FROM Customers WHERE RegionName = 'Bielsko'";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue(id);
+            command.Parameters.AddWithValue(regionName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "";      //ta nazwa
+            using var connection = new SqlConnection(connectionString);
+            var db = new DB();
+            connection.Open();
+
+            db.Select(connection);
+            db.Insert(connection, 10, "Bielsko");
+            db.Delete(connection, 10, "Bielsko");
+
+            connection.Close();
+        }
+    }
+}
